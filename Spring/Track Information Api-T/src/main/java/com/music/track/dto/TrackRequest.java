1ec3c49913b7fd93783a7369efabd16af0255f41
A-DILEEP <dileepalla2003@gmail.com>
2025-09-11 15:57:15 +0530

    Spring

--- Spring/Track Information Api-T/src/main/java/com/music/track/dto/TrackRequest.java
+++
@@ -1,9 +0,0 @@
-package com.music.track.dto;
-
-import java.util.Date;
-
-public record TrackRequest(String title,
-                           String albumName,
-                           Date releaseDate,
-                           Integer playCount) {
-}
